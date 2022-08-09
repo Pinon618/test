39f65da719ea2efeead6633850d646582e33f185
Pinon618 <minhazu618@gmail.com>
2022-08-09 23:44:57 +0600

    hi

---
+++ lab4.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define input freopen("D:/inputoutput/input.txt", "r", stdin);
+#define output freopen("D:/inputoutput/aa.txt", "w",stdout);
+using namespace std;
+
+   strutct node{
+        int data ;
+        strutct node *next;
+   };
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+  input; output;
+#endif
+    
+
+    return 0;
+}
