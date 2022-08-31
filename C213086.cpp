d7d34fa37c3aa5b6cdc361af2a14e987d52f4ace
Pinon618 <minhazu618@gmail.com>
2022-08-31 22:25:11 +0600

    test

--- C213086.cpp
+++ C213086.cpp
@@ -7,31 +7,14 @@ using namespace std;
 #define sort(a) sort(a.begin(),a.end())
 #define rep(i,a,b) for (ll i = a; i < b; i++)
 
+   
 
-    void solve(){
-        ll n;
-        cin>>n;
-        if(n==0 || n==1){
-                cout<<"Not Prime";
-                return;
-            }
-        rep(i,2,n){
-            if(n%(i*i)==0){
-                cout<<"Not Prime";
-                return;
-            }
-        }
-        cout<<"Prime";
-    }
-
-int main()
-{
+int main(){
     #ifndef ONLINE_JUDGE
   input; output;
 #endif
-   int q=1;
-   // cin>>q;
-   while(q--)
-    solve();
-    return 0;
-}
+vector<int> v;
+   cout<<"pinon";
+     
+  return 0;
+ }
